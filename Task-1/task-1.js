cbaf495b4a0b5edb47b9653dd8823c2c5cc2b3da
mehdi80 <mk2954751@gmail.com>
2024-09-09 19:15:47 +0330

    complete: Complete part a of the first task

--- Task-1/task-1.js
+++ Task-1/task-1.js
@@ -1,2 +1,23 @@
 
+const add = (x, y) => {
+    return x + y;
+}
 
+const sub = function (x, y) {
+    return x - y;
+}
+
+function mult(x, y) {
+    return x * y;
+}
+
+const divi = (x, y) => {
+    return x / y;
+}
+
+
+
+console.log(add(5 ,6));
+console.log(sub(6,3));
+console.log(mult(4,8));
+console.log(divi( 18,6));
