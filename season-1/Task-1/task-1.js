e77ca9bd70de94ee2eb76aa7e99df4044647238d
mehdi80 <mk2954751@gmail.com>
2024-09-10 19:39:18 +0330

    refactor: refactoring the first task of section1

--- season-1/Task-1/task-1.js
+++ season-1/Task-1/task-1.js
@@ -1,3 +1,32 @@
+let num1;
+let num2;
+let newDiv = document.createElement('div');
+newDiv.innerHTML = `
+ <form id="form">
+     <label for="number1"> number 1:</lable>
+     <br>
+     <input type="number" id="number1"/>
+     <br/>
+     <br/>
+     <label for="number2"> number 2:</lable>
+     <br>
+     <input type="number" id="number2"/>
+
+     <br/>
+     <br/>
+
+     <button type="submit">Submit</button>
+ </form>
+
+ <p>addition: <span id="addition"></span></p>
+ <p>subtraction: <span id="subtraction"></span></p>
+ <p>multiplication: <span id="multiplication"></span></p>
+ <p>division: <span id="division"></span></p> `
+
+document.body.appendChild(newDiv);
+
+const form = document.getElementById('form');
+form.addEventListener('submit', submit);
 
 const add = (x, y) => {
     return x + y;
@@ -16,8 +45,14 @@ const divi = (x, y) => {
 }
 
 
+function submit(event) {
+    event.preventDefault();
 
-console.log(add(5, 6));
-console.log(sub(6, 3));
-console.log(mult(4, 8));
-console.log(divi(18, 6));
+    num1 = document.getElementById('number1').value;
+    num2 = document.getElementById('number2').value;
+
+    document.getElementById('addition').innerText = add(num1, num2);
+    document.getElementById('subtraction').innerText = sub(num1, num2);
+    document.getElementById('multiplication').innerText = mult(num1, num2);
+    document.getElementById('division').innerText = divi(num1, num2);
+}
