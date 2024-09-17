c374e391732997bc0605aae7a55960c16c11768a
mehdi80 <mk2954751@gmail.com>
2024-09-17 19:16:12 +0330

    create: refactor task 1 to using classes for every calculate methods

--- season-1/Task-1/task-1.js
+++ season-1/Task-1/task-1.js
@@ -1,5 +1,54 @@
-let num1;
-let num2;
+class Parent {
+    constructor(a, b) {
+        this.a = parseFloat(a);
+        this.b = parseFloat(b);
+    }
+}
+
+class Add extends Parent {
+    calculate() {
+        return this.a + this.b;
+    }
+}
+
+class Sub extends Parent {
+    calculate() {
+        return this.a - this.b;
+    }
+}
+
+class Multi extends Parent {
+    calculate() {
+        return this.a * this.b;
+    }
+}
+
+class Divi extends Parent {
+    calculate() {
+        if (this.b === 0) {
+            return "err";
+        }
+        return this.a / this.b;
+    }
+}
+
+class StructureBuilder {
+    constructor() {
+        this.inputs = {};
+    }
+
+    build() {
+        let num1 = document.getElementById('number1').value;
+        let num2 = document.getElementById('number2').value;
+        this.inputs = {
+            a: num1,
+            b: num2,
+        }
+    }
+}
+
+let selectedOperation = null;
+
 let newDiv = document.createElement('div');
 newDiv.innerHTML = `
  <form id="form">
@@ -15,44 +64,62 @@ newDiv.innerHTML = `
      <br/>
      <br/>
 
-     <button type="submit">Submit</button>
+     <button type="submit" id="add-btn">+</button>
+     <button type="submit" id="sub-btn">-</button>
+     <button type="submit" id="mult-btn">*</button>
+     <button type="submit" id="divi-btn">%</button>
+     <button type="submit" id="equal-btn">=</button>
  </form>
 
- <p>addition: <span id="addition"></span></p>
- <p>subtraction: <span id="subtraction"></span></p>
- <p>multiplication: <span id="multiplication"></span></p>
- <p>division: <span id="division"></span></p> `
+ <p id="result"></p> `;
 
 document.body.appendChild(newDiv);
+
+document.getElementById("add-btn").addEventListener('click', () => {
+     selectedOperation = 'addition';
+})
+document.getElementById("sub-btn").addEventListener('click', () => {
+     selectedOperation = 'subtraction';
+})
+document.getElementById("mult-btn").addEventListener('click', () => {
+     selectedOperation = 'multiplication';
+})
+document.getElementById("divi-btn").addEventListener('click', () => {
+     selectedOperation = 'division';
+})
+
 
 const form = document.getElementById('form');
 form.addEventListener('submit', submit);
 
-const add = (x, y) => {
-    return x + y;
-}
-
-const sub = function (x, y) {
-    return x - y;
-}
-
-function mult(x, y) {
-    return x * y;
-}
-
-const divi = (x, y) => {
-    return x / y;
-}
-
-
 function submit(event) {
     event.preventDefault();
 
-    num1 = document.getElementById('number1').value;
-    num2 = document.getElementById('number2').value;
+    const builder = new StructureBuilder;
+     builder.build();
 
-    document.getElementById('addition').innerText = add(num1, num2);
-    document.getElementById('subtraction').innerText = sub(num1, num2);
-    document.getElementById('multiplication').innerText = mult(num1, num2);
-    document.getElementById('division').innerText = divi(num1, num2);
+    let result;
+
+    switch (selectedOperation) {
+        case 'addition':
+            const addition = new Add(builder.inputs.a, builder.inputs.b);
+            result = addition.calculate();
+            break;
+        case 'subtraction':
+            const subtraction =new Sub(builder.inputs.a, builder.inputs.b);
+            result = subtraction.calculate();
+            break;
+        case 'multiplication':
+            const multiplication = new Multi(builder.inputs.a, builder.inputs.b);
+            result = multiplication.calculate();
+            break;
+        case 'division':
+            const division =new Divi(builder.inputs.a, builder.inputs.b);
+            result = division.calculate();
+            break
+        default:
+            result = 'please select an operation';
+            break
+    }
+    document.getElementById('result').innerHTML = result;
 }
